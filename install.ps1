42d5f7b76649308e5cbe22aa7fe050e5f241dd82
69MAR <madityarao5@gmail.com>
2026-03-02 17:07:12 +0530

    Update version in installation scripts from v1.3 to v1.5

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 # Run in PowerShell as Administrator:
 #   irm https://github.com/MAdityaRao/mar-lang/releases/download/v1.4/install.ps1 | iex
 
-$VERSION = "v1.3"
+$VERSION = "v1.5"
 $REPO = "MAdityaRao/mar-lang"
 $URL = "https://github.com/$REPO/releases/download/$VERSION/mar-windows-x64.exe"
 $DEST = "$env:ProgramFiles\Mar"
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="v1.3"
+VERSION="v1.5"
 REPO="MAdityaRao/mar-lang"
 BASE="https://github.com/$REPO/releases/download/$VERSION"
 
